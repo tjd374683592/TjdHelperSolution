3a8082257c05e96dcfd54ed4e3716133f1064843
汤景东 <374683592@qq.com>
2025-02-27 15:44:40 +0800

    add debug

--- TjdHelper/ViewModels/ToolsControlViewModel.cs
+++ TjdHelper/ViewModels/ToolsControlViewModel.cs
@@ -1,28 +1,17 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using TjdHelper.Tools;
-using System;
-using System.Collections.Generic;
+﻿using TjdHelper.Tools;
 using System.ComponentModel;
-using System.IO.Compression;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using System.Windows;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Text.Json.Nodes;
 using System.Drawing;
 using ZXing.Common;
 using ZXing;
-using ZXing.QrCode;
 using ZXing.Windows.Compatibility;
 using System.Windows.Media.Imaging;
 using System.Diagnostics;
 using Microsoft.Win32;
-using TjdHelper.Models;
 
 namespace TjdHelper.ViewModels
 {
@@ -48,8 +37,6 @@ namespace TjdHelper.ViewModels
             DownloadQRCodeCommand = new RelayCommand(DownloadQRCodeCommandExecute);
             DecodeQRCodeCommand = new RelayCommand(DecodeQRCodeCommandExecute);
             ChooseQRCodePathCommand = new RelayCommand(ChooseQRCodePathCommandExecute);
-            CheckWinErrorDetailsCommand = new RelayCommand(CheckWinErrorDetailsCommandExecute);
-            WinErrorAndDetailsClearCommand = new RelayCommand(WinErrorAndDetailsClearCommandExecute);
         }
 
         public ICommand ConvertToTimeCommand { get; private set; }
@@ -63,8 +50,6 @@ namespace TjdHelper.ViewModels
         public ICommand DownloadQRCodeCommand { get; set; }
         public ICommand DecodeQRCodeCommand { get; set; }
         public ICommand ChooseQRCodePathCommand { get; set; }
-        public ICommand CheckWinErrorDetailsCommand { get; set; }
-        public ICommand WinErrorAndDetailsClearCommand { get; set; }
 
         /// <summary>
         /// 绑定时间转换TimeHelper User Control中的txtTime控件WaterMark属性值
@@ -360,42 +345,6 @@ namespace TjdHelper.ViewModels
         }
 
         /// <summary>
-        /// WinError Code字符串
-        /// </summary>
-        private string _strWinErrorCode;
-
-        public string StrWinErrorCode
-        {
-            get { return _strWinErrorCode; }
-            set
-            {
-                if (_strWinErrorCode != value)
-                {
-                    _strWinErrorCode = value;
-                    OnPropertyChanged(nameof(StrWinErrorCode));
-                }
-            }
-        }
-
-        /// <summary>
-        /// WinError Details字符串
-        /// </summary>
-        private string _strWinErrorCodeDetails;
-
-        public string StrWinErrorCodeDetails
-        {
-            get { return _strWinErrorCodeDetails; }
-            set
-            {
-                if (_strWinErrorCodeDetails != value)
-                {
-                    _strWinErrorCodeDetails = value;
-                    OnPropertyChanged(nameof(StrWinErrorCodeDetails));
-                }
-            }
-        }
-
-        /// <summary>
         /// 时间戳转换时间
         /// </summary>
         /// <param name="parameter"></param>
@@ -653,98 +602,6 @@ namespace TjdHelper.ViewModels
                 // 获取文件路径
                 DecodeQRImagePath = openFileDialog.FileName;
             }
-        }
-
-        /// <summary>
-        /// 查询Win Error Code详情
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <exception cref="NotImplementedException"></exception>
-        private void CheckWinErrorDetailsCommandExecute(object obj)
-        {
-            string currentPath = AppDomain.CurrentDomain.BaseDirectory;
-            string exePath = Path.Combine(currentPath, @"Resources\Tools\Err_6.4.5.exe");
-            string errorCodesJsonFilePath = Path.Combine(currentPath, @"Resources\Tools\errorCodes.json");
-            string arguments = StrWinErrorCode;  // 传递的参数
-
-            try
-            {
-                ProcessStartInfo psi = new ProcessStartInfo
-                {
-                    FileName = exePath,
-                    Arguments = arguments,
-                    RedirectStandardOutput = true, // 重定向标准输出
-                    RedirectStandardError = true,  // 重定向错误输出
-                    UseShellExecute = false, // 不使用shell执行
-                    CreateNoWindow = true   // 不创建窗口
-                };
-
-                using (Process process = new Process { StartInfo = psi })
-                {
-                    process.Start();
-
-                    // 读取标准输出
-                    string output = process.StandardOutput.ReadToEnd();
-                    // 读取错误输出
-                    string error = process.StandardError.ReadToEnd();
-                    process.WaitForExit();  // 等待进程结束
-                    int exitCode = process.ExitCode;  // 获取返回的退出代码
-
-
-                    StringBuilder sb = new StringBuilder();
-                    // 读取 JSON 文件 -> 输出中文error info
-                    string jsonContent = File.ReadAllText(errorCodesJsonFilePath);
-                    // 解析 JSON 数据
-                    List<WinErrorInfo> errorList = JsonConvert.DeserializeObject<List<WinErrorInfo>>(jsonContent);
-                    // 查找对应的错误信息
-                    WinErrorInfo errorJsonInfo = new WinErrorInfo();
-                    //null的时候赋值为0
-                    StrWinErrorCode = string.IsNullOrEmpty(StrWinErrorCode) ? "0" : StrWinErrorCode.Trim();
-
-                    //判断用户输入的是10进制还是16进制
-                    if (DecimalHelper.IsDecimal(StrWinErrorCode))
-                    {
-                        errorJsonInfo = errorList.FirstOrDefault(e => e.ErrorCode == Convert.ToInt32(StrWinErrorCode));
-                    }
-                    else if (DecimalHelper.IsHexadecimal(StrWinErrorCode))
-                    {
-                        errorJsonInfo = errorList.FirstOrDefault(e => Convert.ToInt32(e.HexCode, 16) == Convert.ToInt32(StrWinErrorCode, 16));
-                    }
-
-
-                    if (errorJsonInfo != null)
-                    {
-                        sb.AppendLine("Error Code:" + errorJsonInfo.ErrorCode);
-                        sb.AppendLine("Error HexCode:" + errorJsonInfo.HexCode);
-                        sb.AppendLine("Error ErrorName:" + errorJsonInfo.ErrorName);
-                        sb.AppendLine("Error Description:" + errorJsonInfo.Description + "\r\n\r\n");
-                    }
-
-                    //拼装ErrTool输出的内容
-                    sb.AppendLine("Output:");
-                    sb.AppendLine(output);
-                    sb.AppendLine("Error:");
-                    sb.AppendLine(error);
-
-                    StrWinErrorCodeDetails = sb.ToString();
-                }
-            }
-            catch (Exception ex)
-            {
-                ((MainWindowViewModel)Application.Current.MainWindow.DataContext).ShowFlyOut = true;
-                ((MainWindowViewModel)Application.Current.MainWindow.DataContext).LogInfo = "Exception: " + ex.Message;
-            }
-        }
-
-        /// <summary>
-        /// 清空WinError Code和WinError Details
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <exception cref="NotImplementedException"></exception>
-        private void WinErrorAndDetailsClearCommandExecute(object obj)
-        {
-            StrWinErrorCode = "";
-            StrWinErrorCodeDetails = "";
         }
     }
 }
---
+++ TjdHelper/Views/DebugControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TjdHelper.ViewModels;
+
+namespace TjdHelper.Views
+{
+    /// <summary>
+    /// DebugControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class DebugControl : UserControl
+    {
+        public DebugControlViewModel DebugControlVM { get; set; }
+        public DebugControl()
+        {
+            InitializeComponent();
+
+            DebugControlVM = new DebugControlViewModel();
+            DataContext = DebugControlVM;
+        }
+    }
+}
